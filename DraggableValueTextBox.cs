7dbfc7ee6291dd3ca559b594aae486c4d405d622
TacticalHorse <47297421+TacticalHorse@users.noreply.github.com>
2019-02-12 16:04:22 +0500

    Update DraggableValueTextBox.cs
    Import class in your project.
    Change namespace on yours.
    Rebuild your project.
    U can use it.
    <local:DraggableValueTextBox Text="{Binding Some, UpdateSourceTrigger=PropertyChanged, Mode=TwoWay}"/>

--- DraggableValueTextBox.cs
+++ DraggableValueTextBox.cs
@@ -9,18 +9,29 @@ namespace SomeProjectNamespace
     {
         private bool IsDragging;
         protected Point ClickPoint;
-        private bool IsValueParsed;
+        private bool? IsValueParsed = null;
         private int Value;
-        protected override void OnInitialized(EventArgs e)
+        protected override void OnMouseEnter(MouseEventArgs e)
         {
-            base.OnInitialized(e);
-            Focusable = false;
-            Cursor = Cursors.SizeWE;
+            if (IsValueParsed == null)
+            {
+                if (!string.IsNullOrEmpty(Text))
+                {
+                    string var = Text.Replace(',', '.').Split('.')[0];
+                    IsValueParsed = int.TryParse(var, out Value);
+                    if (IsValueParsed == true)
+                    {
+                        Focusable = false;
+                        Cursor = Cursors.SizeWE;
+                    }
+                }
+            }
+            base.OnMouseEnter(e);
         }
 
         protected override void OnPreviewMouseMove(MouseEventArgs e)
         {
-            if (IsDragging && IsValueParsed)
+            if (IsDragging && IsValueParsed == true)
             {
                 Point p = e.GetPosition(this);
                 if (p.X > ClickPoint.X)
@@ -36,23 +47,22 @@ namespace SomeProjectNamespace
 
         protected override void OnMouseDown(MouseButtonEventArgs e)
         {
-            if (!IsFocused)
+            if (!IsFocused && IsValueParsed == true)
             {
                 IsDragging = true;
                 ClickPoint = e.GetPosition(this);
-                IsValueParsed = int.TryParse(Text, out Value);
                 Mouse.Capture(this);
-                Console.WriteLine(Text + "  " + Value);
             }
             base.OnMouseDown(e);
         }
 
         protected override void OnMouseUp(MouseButtonEventArgs e)
         {
-            if (IsDragging && IsValueParsed)
+            if (IsDragging && IsValueParsed == true)
             {
                 IsDragging = false;
                 Point p = e.GetPosition(this);
+                int.TryParse(Text, out Value);
                 if (Math.Abs(p.X - ClickPoint.X) < 2)
                 {
                     Focusable = true;
@@ -60,16 +70,24 @@ namespace SomeProjectNamespace
                     Cursor = Cursors.IBeam;
                     CaretIndex = Text.Length;
                 }
-                ReleaseMouseCapture();
             }
+            else
+            {
+                Focusable = true;
+                Focus();
+            }
+            ReleaseMouseCapture();
             base.OnMouseUp(e);
         }
 
         protected override void OnLostFocus(RoutedEventArgs e)
         {
             base.OnLostFocus(e);
-            Cursor = Cursors.SizeWE;
-            Focusable = false;
+            if (IsValueParsed == true)
+            {
+                Cursor = Cursors.SizeWE;
+                Focusable = false;
+            }
         }
     }
 }
